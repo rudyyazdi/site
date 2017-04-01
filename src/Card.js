02e28fd1eaaeb45ca06f0a1856b6b8a0c6a2b969
rudyyazdi <rudyyazdi@gmail.com>
2017-04-01 23:53:45 +1100

    added card flip capability

--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,36 @@
 import React, { PropTypes } from 'react';
 
-const style = {
+const defaultStyle = {
   width: 100,
-  height: 100,
-  backgroundColor: 'red'
+  height: 75,
+  fontFamily: 'monospace',
+  fontSize: 40,
+  textAlign: 'center',
+  paddingTop: 25,
+  border: '1px doted',
+  cursor: 'pointer'
 };
 
-const Card = ({ id }) => (
-  <div style={style}>
-    {id}
-  </div>
-);
+const Card = ({ id, isVisible }) => {
+  const style = Object.assign(defaultStyle);
+  if (isVisible) {
+    style.backgroundColor = 'white';
+  } else {
+    style.backgroundColor = 'black';
+  }
+  return (
+    <div style={style}>
+      {id}
+    </div>
+  );
+};
 
 Card.propTypes = {
-  id: PropTypes.number.isRequired
+  id: PropTypes.number.isRequired,
+  isVisible: PropTypes.bool
+};
+Card.defaultProps = {
+  isVisible: false
 };
 
 export default Card;
