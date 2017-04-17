9de4005ec886d956fb2fa08ab7d5667af573a466
rudyyazdi <rudyyazdi@gmail.com>
2017-04-17 15:19:08 +1000

    added html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './main.js',
   output: {
     path: path.join(__dirname, '/rudyyazdi.github.io'),
-    publicPath: '/rudyyazdi.github.io/',
+    // publicPath: '/rudyyazdi.github.io/',
     filename: 'bundle.js',
   },
   devServer: {
     inline: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.esj',
+    }),
+  ],
   module: {
     loaders: [
       {
